4843d2c006135bf3d232733b25b68f759f528f81
WEP-56 <1484413790@qq.com>
2026-02-06 22:57:50 +0800

    删除了一些学习参考用的代码

--- Browser-AI-main/comet-browser/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from 'next/dynamic'
-
-const ClientOnlyPage = dynamic(() => import('./ClientOnlyPage'), { ssr: false })
-
-export default function Page() {
-  return <ClientOnlyPage />
-}
--- Browser-AI-main/comet-browser/src/lib/llm/providers/base.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// src/lib/llm/providers/base.ts
-Object.defineProperty(exports, "__esModule", { value: true });
